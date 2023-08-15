b4aa1d6190226b8898f761791841c52b624f7f82
Lis de Lima Candeia <liscandeia523@gmail.com>
2023-08-15 02:48:56 +0000

    Classes + matriz

--- src/main/java/com/example/AppClasses.java
+++ src/main/java/com/example/AppClasses.java
@@ -1,5 +1,7 @@
 package com.example;
 
+import java.util.Scanner;
+
 public class AppClasses {
     public static void main(String[] args) {
        /* System.out.println("Criando classes");
@@ -17,7 +19,18 @@ public class AppClasses {
         System.out.println("Ano de nascimento: " + cliente1.getAnoNa()+ " Renda: "+cliente1.getRenda()+" Sexo: "+cliente1.getSexo() + " é especial? " + cliente1.isEspecial());
         Cliente cliente2 = new Cliente(10000,'F',2001,"Lis", "05505100000");
         System.out.println("Ano de nascimento: " + cliente2.getAnoNa()+ " Renda: "+cliente2.getRenda()+" Sexo: "+cliente2.getSexo() + " é especial? " + cliente2.isEspecial());
+        
+        //testando scanner para implementar
 
+        Scanner scanner = new Scanner(System.in);
+        Cliente cliente3 = new Cliente();
+        System.out.println("Digite a data de nascimento");
+        cliente3.setAnoNa(scanner.nextInt());
+        scanner.nextLine();
+        System.out.println("Digite seu nome");
+        cliente3.setNome(scanner.nextLine());
+        
+        System.out.println(cliente3.getNome()); 
 
 
     }
